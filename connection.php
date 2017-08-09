545e761e3ac61dfdece75ec0087997473951b0ac
rammanoj <rammanojpotla1608@gmail.com>
2017-08-09 11:49:59 +0000

    connect to database and login

---
+++ connection.php
@@ -0,0 +1,15 @@
+<?php
+
+#connect to your datbase by creating a new PDO object 
+#if no errors encountered it displays a blank page
+
+try{
+#try catch blocks are used to identify the error and give it
+#the below line creates a new pdo object which is used in conncting database
+	$connection = new PDO('mysql:host=localhost;dbname=signup;charset=utf8mb4', 'root', '');
+	$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}catch(PDOException $err){
+	echo $err->getMessage();
+	die();
+}
+?>
