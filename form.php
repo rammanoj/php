ca74345e51f5fae9dff9d54d1165b564fd121eb2
rammanoj <rammanojpotla1608@gmail.com>
2017-08-30 18:06:55 +0530

    basic form to input the data

---
+++ form.php
@@ -0,0 +1,38 @@
+<html>
+<body>
+<form method="POST" action="register.php">
+<b>Enter your username :  </b><br>
+<input type ="text" name = "username" placeholder = "username" ></br>
+<b>Enter your pasword :  </b><br>
+<input type ="password" name = "password"></br>
+<b>Re-type Password :  </b><br>
+<input type ="password" name = "repass"></br>
+<b>e-mail :  </b><br>
+<input type ="text" name = "email"></br>
+<select name="date">
+<option value="1">1</option><option value="2">2</option><option value="3">3</option><option value="4">4</option><option value="5">5</option>
+<option value="6">6</option><option value="7">7</option><option value="8">8</option><option value="9">9</option><option value="10">10</option>
+<option value="11">11</option><option value="12">12</option><option value="13">13</option><option value="14">14</option><option value="15">15</option>
+<option value="16">16</option><option value="17">17</option><option value="18">18</option><option value="19">19</option><option value="20">20</option>
+<option value="21">21</option><option value="22">22</option><option value="23">23</option><option value="24">24</option><option value="25">25</option>
+<option value="26">26</option><option value="27">27</option><option value="28">28</option><option value="29">29</option><option value="30">30</option>
+</select>
+<select name="month">
+<option value="january">january</option><option value="febrauary">febrauary</option><option value="march">march</option>
+<option value="april">april</option><option value="may">may</option><option value="june">june</option>
+<option value="july">july</option><option value="august">august</option><option value="september">september</option>
+<option value="october">october</option><option value="november">november</option><option value="december">december</option>
+</select>
+<select name="year">
+<option value="2010">1970</option>
+<option value="2011">1971</option>
+<option value="2012">1972</option>
+<option value="2013">1973</option>
+<option value="2014">1978</option>
+<option value="2015">1979</option>
+</select><br>
+<input type = "submit">
+</form>
+</body>
+</html>
+
